c81bc44798c229faf95226b64f07c2e81bb5c51e
Nikolay Panev <panevnikolay@gmail.com>
2025-03-19 14:55:06 +0100

    added numpy notebooks and empty py files for exercises

--- Python/sandbox.py
+++ Python/sandbox.py
@@ -1,7 +1,22 @@
-a = 1 
-def do(x): 
-    a = 100 
-    return x + a 
+# Python Program to Count words in a String using Dictionary
+def freq(string,passedkey):
 
+    #step1: A list variable is declared and initialized to an empty list.
+    words = []
 
-print(do(1))
+    #step2: Break the string into list of words
+    words = string.split() # or string.lower().split()
+
+    #step3: Declare a dictionary
+    Dict = {}
+
+    #step4: Use for loop to iterate words and values to the dictionary
+    for key in words:
+        if(key == passedkey):
+            Dict[key] = words.count(key)   
+    #step5: Print the dictionary
+    print("Total Count:", Dict)
+
+#step6: Call function and pass string in it
+freq("Mary had a little lamb Little lamb, little lamb Mary had a little lamb.Its fleece was white as snow And everywhere that Mary went Mary went, Mary went \
+Everywhere that Mary went The lamb was sure to go","little")
